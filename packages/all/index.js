97aab659f8859800616b09d6ff5dc08f222cbf58
depeng.kong <15365182986@163.com>
2021-06-18 22:42:35 +0800

    feat: init

---
+++ packages/all/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: [
+    '@depeng9527/eslint-config-react',
+    '@depeng9527/eslint-config-vue',
+  ],
+}
---
+++ packages/vue/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@depng9527/eslint-config-ts',
+  ],
+  rules: {
+    'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
+    'vue/html-self-closing': 'off',
+    'vue/no-v-html': 'off',
+  },
+}
